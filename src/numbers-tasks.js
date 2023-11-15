4af8219bf71432bf2b9debb8a8fbbef221baa968
Tuychiev Otabek <otabekotabek996996@gmail.com>
2023-11-16 00:08:20 +0500

    docs: solve the problem get sum of digits

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -293,8 +293,11 @@ function getSumToN(n) {
  *   202 => 4  // (2+0+2)
  *   5   => 5  // 5
  */
-function getSumOfDigits(/* num */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(num) {
+  let sum = 0;
+  const numbers = `${num}`.split('');
+  for (let i = 0; i < numbers.length; i += 1) sum += +numbers[i];
+  return sum;
 }
 
 /**
