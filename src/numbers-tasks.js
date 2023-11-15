6121ef308c608ed3397355bdea2d0defc39d7689
Tuychiev Otabek <otabekotabek996996@gmail.com>
2023-11-15 23:15:05 +0500

    docs: solve the problem is prime

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -203,8 +203,12 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  const squareRootOfNumber = Math.round(Math.sqrt(n));
+  for (let i = 2; i <= squareRootOfNumber; i += 1) {
+    if (Number.isInteger(n / i)) return false;
+  }
+  return true;
 }
 
 /**
